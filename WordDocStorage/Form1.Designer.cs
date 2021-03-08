437dc8aa36752129b85bfaf40d659dc250ecff18
Uzair <uzair_mughal20@yahoo.com>
2021-03-09 00:50:13 +0500

    update

---
+++ WordDocStorage/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace WordDocStorage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.dgvDocuments = new System.Windows.Forms.DataGridView();
+            this.btnOpen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocuments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(690, 33);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(94, 30);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(793, 33);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 30);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(37, 33);
+            this.txtFilePath.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(644, 28);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // dgvDocuments
+            // 
+            this.dgvDocuments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDocuments.Location = new System.Drawing.Point(37, 71);
+            this.dgvDocuments.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvDocuments.Name = "dgvDocuments";
+            this.dgvDocuments.RowHeadersWidth = 51;
+            this.dgvDocuments.RowTemplate.Height = 24;
+            this.dgvDocuments.Size = new System.Drawing.Size(849, 475);
+            this.dgvDocuments.TabIndex = 3;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(37, 554);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(94, 30);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 613);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.dgvDocuments);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnBrowse);
+            this.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocuments)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.DataGridView dgvDocuments;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ WordDocStorage/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordDocStorage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.ShowDialog();
+            txtFilePath.Text = dlg.FileName;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFile(txtFilePath.Text);
+            MessageBox.Show("Saved");
+            LoadData();
+            txtFilePath.Text = "";
+        }
+
+        private void SaveFile(string filepath)
+        {
+            using(Stream stream = File.OpenRead(filepath))
+            {
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+
+                var fi = new FileInfo(filepath);
+                string extn = fi.Extension;
+                if (extn == ".docx")
+                    extn = ".doc";
+                string name = fi.Name;
+
+                string query = "INSERT INTO Documents (FileName,Data,Extension) Values(@name,@data,@extn)";
+                 
+                using(SqlConnection cn = GetConnection())
+                {
+                    SqlCommand cmd = new SqlCommand(query, cn);
+                    cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = name;
+                    cmd.Parameters.Add("@data", SqlDbType.VarBinary).Value = buffer;
+                    cmd.Parameters.Add("@extn", SqlDbType.Char).Value = extn;
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection cn = GetConnection())
+            {
+                string query = "SELECT ID,FileName,Extension From Documents";
+                SqlDataAdapter adp = new SqlDataAdapter(query, cn);
+                DataTable dt = new DataTable();
+                adp.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    dgvDocuments.DataSource = dt;
+                }
+
+                dgvDocuments.Columns[1].Width = 600;
+            }
+        }
+
+        
+        private SqlConnection GetConnection()
+        {
+            return new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Free Lancing\Fiverr\tnwitua\WordDocStorage\WordDocStorage\Documents.mdf;Integrated Security=True;Connect Timeout=30");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            var selectedRow = dgvDocuments.SelectedRows;
+            foreach(var row in selectedRow)
+            {
+                int id = (int)((DataGridViewRow)row).Cells[0].Value;
+                OpenFile(id);
+            }
+        }
+        private void OpenFile(int id)
+        {
+            using (SqlConnection cn = GetConnection())
+            {
+                string query = "SELECT Data,FileName,Extension From Documents where ID = @id";
+                SqlCommand cmd = new SqlCommand(query, cn);
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                cn.Open();
+                var reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    var name = reader["FileName"].ToString();
+                    var data = (byte[])reader["data"];
+                    var extn = reader["Extension"].ToString();
+                    var newFilename = name + extn;
+                    File.WriteAllBytes(newFilename, data);
+                    System.Diagnostics.Process.Start(newFilename);
+
+                }
+            }
+        }
+
+    }
+}
